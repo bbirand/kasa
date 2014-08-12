2ff8de74a061be3572e3860cdc241f95da677ed5
Berk Birand <berk@berkbirand.com>
2014-08-12 14:51:11 +0000

    Refactor SensorTag for continuous connection

--- devices/sensortag.py
+++ devices/sensortag.py
@@ -16,10 +16,12 @@
 # in regular expressions, e.g. [ and ] must be escaped with a backslash.
 #
 
+import gevent
+from gevent.queue import Queue
 import pexpect
+
 import sys
 import threading
-import gevent
 import time
 import re
 from IPython.utils.traitlets import Unicode, Float # Used to declare attributes of our widget
@@ -29,6 +31,9 @@ class SensorTag(object):
     def __init__(self, addr):
         ''' Construct with BT address '''
         self._bluetooth_adr = addr
+
+        # Initiate a connection with the tag
+        self._connect_to_tag()
 
         # Initiate all the sensors
         self.temperature = SensorTagTemperature(self)
@@ -68,10 +73,111 @@ class SensorTag(object):
         ''' Find the SensorTag device by MAC'''
         return SensorTag(name) 
 
-
     '''
     Instance methods
     '''
+    @staticmethod
+    def _manager_greenlet(bluetooth_addr, q_in, q_out):
+        '''
+        Greenlet that establishes a connection and waits for input 
+        from the queues
+
+        The items on q_in should be tuples, where the first element is the command:
+        ('read',ctrl_add, read_addr, enable_cmd, disable_cmd)
+        ('quit')
+        '''
+        try:
+            gatt = pexpect.spawn('gatttool -b ' + bluetooth_addr + ' --interactive')
+            gatt.expect('\[LE\]>')
+            #print "Preparing to connect. You might need to press the side button..."
+            gatt.sendline('connect')
+            # test for success of connect
+            gatt.expect('\[CON\].*>', timeout=3)
+            # TODOSet switch to connected
+            q_out.put('ok')
+
+        except pexpect.TIMEOUT:
+            print "Cannot connect to device. Is it discoverable?"
+            gatt.close()
+            q_out.put('error')
+            return 
+
+        # At this point, we should be connected 
+        while True:
+            # Get the command
+            try:
+                cmd = q_in.get(timeout=120)
+            except gevent.queue.Empty:
+                # There were no items in the queue for 120 seconds
+                # Make sure that we're still connected
+                # TODO Check that the prompt says "CON", otherwise run the 
+                # "connect" command
+                continue
+
+            if cmd[0] == "read":
+                # Take a single reading
+                ctrl_addr, read_addr, enable_cmd, disable_cmd = cmd[1:]
+                gatt.sendline('char-write-cmd {} {}'.format(ctrl_addr, enable_cmd))
+                gatt.expect('\[LE\]>')
+                gevent.sleep(1)
+                gatt.sendline('char-read-hnd {}'.format(read_addr))
+                gatt.expect('descriptor: (?P<value>.*) \r\n') 
+                rval = gatt.match.group('value')
+                gatt.expect('\[LE\]>')
+                gatt.sendline('char-write-cmd {} {}'.format(ctrl_addr, disable_cmd))
+                q_out.put(rval)
+
+            elif cmd[0] == "quit":
+                print "Quitting greenlet"
+                break
+
+            elif cmd[0] == 'status':
+                try:
+                    gatt.sendline('  ')
+                    gatt.expect('\[CON\].*>', timeout=3)
+                    q_out.put(True)
+                except pexpect.TIMEOUT:
+                    print "Cannot connect to device."
+                    q_out.put(False)
+                    break
+            else:
+                #TODO What if don't recognize the command
+                pass
+
+        # Close the connection
+        gatt.close()
+
+    def is_connected(self):
+        ''' Check if we're connected
+        '''
+        # the connect method
+        self._mgr_in.put(('status',))
+
+        try:
+            return self._mgr_out.get(timeout=5)
+        except gevent.queue.Empty:
+            return False
+
+    def _connect_to_tag(self):
+        '''Establishes a connection to the sensortag via pexpect
+        Launches a new greenlet that establishes a connection and waits
+        for user input.
+        '''
+
+        # Queue for sending data to the ST manager
+        self._mgr_in = Queue()
+
+        # Q for getting stuff out of the manager (such as readings). All
+        # listening functions should subscribe to this queue
+        self._mgr_out = Queue()
+
+        # Spawn the greenlet
+        self._mgr_greenlet = gevent.spawn(self._manager_greenlet, self._bluetooth_adr,  self._mgr_in, self._mgr_out)
+        print self._mgr_greenlet
+
+        # Wait to hear back whether we got connected
+        print self._mgr_out.get()
+
     def read_value(self, ctrl_addr = '0x29', read_addr = '0x25', enable_cmd = '01', disable_cmd = '00' ):
         ''' Uses the GATT interface to read a value
 
@@ -81,33 +187,12 @@ class SensorTag(object):
         Finally writes `disable_cmd` to `ctrl_addr`
         '''
 
-        rval = ""
-        try:
-            gatt = pexpect.spawn('gatttool -b ' + self._bluetooth_adr + ' --interactive')
-            gatt.expect('\[LE\]>')
-            #print "Preparing to connect. You might need to press the side button..."
-            gatt.sendline('connect')
-            # test for success of connect
-            gatt.expect('\[CON\].*>', timeout=3)
-            gatt.sendline('char-write-cmd {} {}'.format(ctrl_addr, enable_cmd))
-            gatt.expect('\[LE\]>')
-            gevent.sleep(1)
-            gatt.sendline('char-read-hnd {}'.format(read_addr))
-            gatt.expect('descriptor: (?P<value>.*) \r\n') 
-            rval = gatt.match.group('value')
-            gatt.expect('\[LE\]>')
-            gatt.sendline('char-write-cmd {} {}'.format(ctrl_addr, disable_cmd))
-            gatt.expect('\[LE\]>')
+        #TODO Check that we have an active connection ot the manager greenlet, otherwise call
+        # the connect method
+        self._mgr_in.put(('read', '0x29', '0x25', '01', '00'))
+        return self._mgr_out.get(timeout=3)
 
-        except pexpect.TIMEOUT:
-            print "Cannot connect to device. Is it discoverable?"
-        finally:
-            gatt.close()
-
-        return rval
-
-class SensorTagTemperature(object):
-#class SensorTagTemperature(TemperatureWidget):
+class SensorTagTemperature(TemperatureWidget):
     def __init__(self, sensortag):
         ''' Construct with the object of the corresponding sensortag '''
         self.sensortag = sensortag
@@ -231,7 +316,29 @@ class SensorTagTemperature(object):
         # Must run this in a new thread so that it can coexist with the IPython code
         self._plotly_poller = threading.Thread(target=poller).start()
 
+    def new_update_every(self, secs=5):
+        def poller():
+            while True:
+                self.read()
+                gevent.sleep(secs)
+        self._new_poller = threading.Thread(target=poller).start()
+        print self._new_poller 
+
     def update_every(self, secs=5):
+        def poller():
+            while True:
+                self.read()
+                gevent.sleep(secs)
+
+        # Must run this in a new thread so that it can coexist with the IPython code
+        self._poller = gevent.spawn(poller)
+
+    def stop_poll(self):
+        # Kill the polling updater greenlet
+        self._poller.kill()
+
+
+    def old_update_every(self, secs=5):
         ''' Update the temperature every x secs
         Default period is 5 seconds.
         '''
@@ -253,7 +360,7 @@ class SensorTagTemperature(object):
         # Keep reference to the thread to be able to kill it later
         self._poller = t
 
-    def stop_poll(self):
+    def old_stop_poll(self):
         ''' Stop updating the SensorTag '''
         self._continue_update = False
         #To make sure that the thread doesn't hang?
