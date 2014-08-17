af9b1f329da36349c9dcc4205580d6d2f1fffedb
Berk Birand <git@berkbirand.com>
2014-08-17 20:36:53 +0000

    Add CLI tester for bt_gatt

---
+++ daemons/bt_gatt_test.py
@@ -0,0 +1,19 @@
+import zmq
+import sys
+
+def main():
+
+    # Connect to the broker as a client
+    port = "9800"
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:%s" % port)
+
+    print "Sending request:'{}'".format(" ".join(sys.argv[1:]))
+    socket.send("GATT " + " ".join(sys.argv[1:]))
+
+    message = socket.recv_multipart()
+    print "Received reply:'{}'".format(message)
+
+
+if __name__=="__main__": main()
