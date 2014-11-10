55d571ed218d5b9ffb0251ca96d9945a37306572
Berk Birand <git@berkbirand.com>
2014-11-09 19:26:09 -0500

    Moved functions outside of class

--- kasa.py
+++ kasa.py
@@ -1,71 +1,82 @@
 #/usr/bin/env python
 
+'''
+
+Kasa utility functions for discovery, visuals etc..
+
+(c) 2014 Berk Birand
+
+'''
+
 #TODO Load all device drivers (custom import?)
 from devices.wemo import WeMoSwitch
 from devices.sensortag import SensorTag
 
 from devices.utils import raise_msg
 
-class Kasa(object):
+# Cache for discovered devices
+devs = []
 
-    def __init__(self):
-        # Place to keep all devices
-        self.devs = []
+def kasa(name):
+    ''' Find the device by name'''
+    
+    #TODO: If device is already created, return that device
 
-    def __getitem__(self, name):
-        ''' Find the device by name'''
-        
-        # Look at all the devices we have in the cache
-        for d in self.devs:
-            # If there is match return it
-            if d[1] == name:
-                return d[0].get_device(name)
-        # If there was no match, raise exception
-        raise_msg( LookupError("Device '{}' not available.".format(name)) )
+    # Look at all the devices we have in the cache
+    for d in devs:
+        # If there is match return it
+        if d[1] == name:
+            return d[0].get_device(name)
+    # If there was no match, raise exception
+    raise_msg( LookupError("Device '{}' not available.".format(name)) )
 
-    def _pretty_print_devs(self):
-        ''' Look at self.devs and pprint '''
 
-        from IPython.display import HTML
+'''
 
-        ht = """<div class="discovered"><table class="flat-table flat-table-3">
-        <tr>
-        <th>Name</th>
-        <th>Type</th>
-        </tr>
-        """
+ Discovery functions
 
-        #template = "{0:10}|{0:}"
-        #print template.format("Type","Name")
-        #print "-----------------"
-        for i in self.devs:
-            #print template.format(i[0].pretty_name(), i[1])
-            ht += "<tr><td>{}</td><td>{}</td></tr>".format(i[1], i[0].pretty_name())
+'''
+def _pretty_print_devs():
+    ''' Look at devs and pprint '''
 
-        ht += "</table></div>"
-        return HTML(ht)
+    from IPython.display import HTML
 
-    def discover(self, pprint = True):
-        '''
-        Return devices if anything was discovered
+    ht = """<div class="discovered"><table class="flat-table flat-table-3">
+    <tr>
+    <th>Name</th>
+    <th>Type</th>
+    </tr>
+    """
 
-        Arguments:
-         pprint: if True, also print the devices
-        '''
+    #template = "{0:10}|{0:}"
+    #print template.format("Type","Name")
+    #print "-----------------"
+    for i in devs:
+        #print template.format(i[0].pretty_name(), i[1])
+        ht += "<tr><td>{}</td><td>{}</td></tr>".format(i[1], i[0].pretty_name())
 
-        # This would be reset once
-        self.devs = []
+    ht += "</table></div>"
+    return HTML(ht)
 
-        #TODO: Dynamically get installed modules
-        for a in [WeMoSwitch, SensorTag]:
-            #TODO Spawn each discover function in its own greenlet
-            res = a.discover()
-            if res:
-                self.devs.extend(res)
+def discover(pprint = True):
+    '''
+    Return devices if anything was discovered
 
-        #Pretty Print list of devices
-        if pprint:
-            return self._pretty_print_devs()
+    Arguments:
+     pprint: if True, also print the devices
+    '''
 
-# Create an instance of the entire environment
-kasa = Kasa()
+    # Reset devs before every discover
+    global devs
+    devs = []
+
+    #TODO: Dynamically get installed modules
+    for a in [WeMoSwitch, SensorTag]:
+        #TODO Spawn each discover function in its own greenlet
+        res = a.discover()
+        if res:
+            devs.extend(res)
+
+    #Pretty Print list of devices
+    if pprint:
+        return _pretty_print_devs()
